00d946fa06079fd65f2c782b238cf8af602c275d
Ahmad Software <dr.ahmad.salah.54@gmail.com>
2024-04-30 17:05:15 +0300

    change databse name

--- database/knexfile.js
+++ database/knexfile.js
@@ -5,7 +5,7 @@ const development = {
     port: 5432,
     user: "postgres",
     password: "aghlqtyo2018#",
-    database: "cs_database",
+    database: "pet_system",
   },
   seeds: {
     path: "/seeds",
@@ -19,7 +19,7 @@ const production = {
     port: 5432,
     user: "postgres",
     password: "aghlqtyo2018#",
-    database: "cs_database",
+    database: "pet_system",
   },
   seeds: {
     path: "/seeds",
